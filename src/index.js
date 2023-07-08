cb16085b21558aa140a37744966e4b4119be99fd
TROLLGAMES <84880155+GsLKS@users.noreply.github.com>
2023-07-08 11:39:43 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/*
+ * Cubie / CubieCloud / Aethera / GsLKS (c) 2019 - 2023 - All Right Reserved
+ * Cubie: cloud.cubie.com.br / cubiecloud.ml 
+ * 
+ * Aethera (c) 2023
+ * AsXCore/AsX-Neural (c) 2023
+ * IEFW/CubieCloud IE (c) 2023
+*/
+
+exports.AsXCore = require("../libs/aethera/asxcore");
+exports.AntiError = require("../libs/aethera/anti-error");
+exports.MinJS = require("../libs/aethera/min.js");
+exports.IEFW = require("../libs/gslks/iefw");
+exports.IEFWNative = require("../libs/aethera/iefw-native");
